20e8c90aad2f7974a33f920e5e8cdefb936bf36a
Burak <60696929+Burak-san@users.noreply.github.com>
2022-08-13 14:59:04 +0300

    Input Manager Scene Integrated

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -3,6 +3,8 @@ using Data.UnityObjects;
 using Data.ValueObjects;
 using Keys;
 using Signals;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 namespace Managers
 {
@@ -24,8 +26,7 @@ namespace Managers
 
         #region Private Variables
 
-        private bool _isTouching;
-
+        private bool _isTouching; //ref type
         private float _currentVelocity; //ref type
         private Vector2? _mousePosition; //ref type
         private Vector3 _moveVector; //ref type
@@ -47,23 +48,111 @@ namespace Managers
         {
             SubscribeEvents();
         }
+
         private void SubscribeEvents()
         {
             InputSignals.Instance.onEnableInput += OnEnableInput;
             InputSignals.Instance.onDisableInput += OnDisableInput;
-            
+            CoreGameSignals.Instance.onPlay += OnPlay;
+            CoreGameSignals.Instance.onReset += OnReset;
         }
 
         private void UnSubscribeEvents()
         {
-
+            InputSignals.Instance.onEnableInput -= OnEnableInput;
+            InputSignals.Instance.onDisableInput -= OnDisableInput;
+            CoreGameSignals.Instance.onPlay -= OnPlay;
+            CoreGameSignals.Instance.onReset -= OnReset;
         }
+
         private void OnDisable()
         {
-            UnSubscribeEvents;
+            UnSubscribeEvents();
         }
 
         #endregion
+
+        private void Update()
+        {
+            if (!isReadyForTouch) return;
+
+            if (Input.GetMouseButtonUp(0) && !IsPointerOverUIElement())
+            {
+                EndMouseDrag();
+            }
+
+
+            if (Input.GetMouseButtonDown(0) && !IsPointerOverUIElement())
+            {
+                StartMouseDrag();
+            }
+
+            if (Input.GetMouseButton(0) && !IsPointerOverUIElement())
+            {
+                if (_isTouching)
+                {
+                    if (_mousePosition != null)
+                    {
+                        MouseDrag();
+                    }
+                }
+            }
+        }
+
+        #region Mouse Drag Methods
+        private void EndMouseDrag()
+        {
+            _isTouching = false;
+            InputSignals.Instance.onInputReleased?.Invoke();
+        }
+
+        private void StartMouseDrag()
+        {
+            _isTouching = true;
+            InputSignals.Instance.onInputTaken?.Invoke();
+            if (!isFirstTimeTouchTaken)
+            {
+                isFirstTimeTouchTaken = true;
+                InputSignals.Instance.onFirstTimeTouchTaken?.Invoke();
+            }
+
+            _mousePosition = Input.mousePosition;
+        }
+
+        private void MouseDrag()
+        {
+            Vector2 mouseDeltaPos = (Vector2)Input.mousePosition - _mousePosition.Value;
+
+
+            if (mouseDeltaPos.x > Data.HorizontalInputSpeed)
+                _moveVector.x = Data.HorizontalInputSpeed / 10f * mouseDeltaPos.x;
+            else if (mouseDeltaPos.x < -Data.HorizontalInputSpeed)
+                _moveVector.x = -Data.HorizontalInputSpeed / 10f * -mouseDeltaPos.x;
+            else
+                _moveVector.x = Mathf.SmoothDamp(_moveVector.x, 0f, ref _currentVelocity,
+                    Data.ClampSpeed);
+
+            _mousePosition = Input.mousePosition;
+
+            InputSignals.Instance.onInputDragged?.Invoke(new HorizontalInputParams()
+            {
+                XValue = _moveVector.x,
+                ClampValues = new Vector2(Data.ClampSides.x, Data.ClampSides.y)
+            });
+        }
+
+        private bool IsPointerOverUIElement()
+        {
+            var eventData = new PointerEventData(EventSystem.current);
+            eventData.position = Input.mousePosition;
+            var results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, results);
+            return results.Count > 0;
+        }
+        #endregion
+
+
+        #region Subscribed Methods
 
         private void OnEnableInput()
         {
@@ -80,8 +169,12 @@ namespace Managers
             isReadyForTouch = true;
         }
 
-
-
+        private void OnReset()
+        {
+            _isTouching = false;
+            isReadyForTouch = false;
+            isFirstTimeTouchTaken = false;
+        }
+        #endregion
     }
 }
-
