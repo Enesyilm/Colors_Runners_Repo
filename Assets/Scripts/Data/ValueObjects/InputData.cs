db6dde9b0d167ab4235ff43e252a33b578dce263
Mehmet Salih ÖNCÜ <oncbilisim@gmail.com>
2022-08-30 16:24:15 +0300

    InputManager and signal update

--- Assets/Scripts/Data/ValueObjects/InputData.cs
+++ Assets/Scripts/Data/ValueObjects/InputData.cs
@@ -10,5 +10,6 @@ namespace Data.ValueObjects
         public float InputSpeed = 2f;
         public Vector2 ClampSides = new Vector2(-3, 3);
         public float ClampSpeed = 0.007f;
+        public float IdleInputSpeed = 1.161f;
     }
 }
